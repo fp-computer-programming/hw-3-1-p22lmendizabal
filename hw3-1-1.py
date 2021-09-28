5001981fc3698312880c8331dd7967606579ae1c
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-09-28 10:19:54 -0400

    hw3-1-1

---
+++ hw3-1-1.py
@@ -0,0 +1,6 @@
+# author: LM (AMDG) 09/28/2021
+
+games_won_first_team = input("PLease enter the number of wins the first team won. ")
+games_tied = input("Please enter the number of games tied. ")
+games_won_second_team = input("Please enter the number of games the second team won. ")
+

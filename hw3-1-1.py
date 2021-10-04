7fed78a149eb76a08712ec27228260e8821d6cd1
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2021-10-04 10:20:33 -0400

    Continue hw 3-1-1

--- hw3-1-1.py
+++ hw3-1-1.py
@@ -4,3 +4,24 @@ games_won_first_team = input("PLease enter the number of wins the first team won
 games_tied = input("Please enter the number of games tied. ")
 games_won_second_team = input("Please enter the number of games the second team won. ")
 
+total_winning_points_first_team = int(games_won_first_team) * 3
+total_winning_points_second_team = int(games_won_second_team) * 3
+total_tied = int(games_tied)
+
+total_points_second_team = total_winning_points_second_team + int(games_tied)
+
+total_points_first_team = total_winning_points_first_team + int(games_tied)
+
+if total_points_first_team > total_points_second_team:
+    print("The first team won the group stage")
+if total_points_first_team < total_points_second_team:
+    print("The second team won the group stage.")
+else:
+    print("Both teams had the same amount of points")
+
+if total_points_first_team > total_points_second_team:
+    print("The first team has " + str(total_points_first_team) + "amount of points.")
+elif total_points_first_team < total_points_second_team:
+    print("The second team has " + str(total_points_first_team) + "amount of points.")
+else:
+    print("Both teams had " + str(tots;))
